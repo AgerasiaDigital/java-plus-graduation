e26c81e6ff37d27359e72a651301df8f58d9be8d
Oleg Digital <boss@agerasiadigital.ru>
2026-04-05 15:17:47 +0300

    jrpc fix

---
+++ stat/analyzer/src/main/java/ru/practicum/analyzer/config/GrpcPortEurekaMetadataRegistrar.java
@@ -0,0 +1,27 @@
+package ru.practicum.analyzer.config;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import net.devh.boot.grpc.server.event.GrpcServerStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class GrpcPortEurekaMetadataRegistrar {
+
+    private final ApplicationInfoManager applicationInfoManager;
+
+    public GrpcPortEurekaMetadataRegistrar(ApplicationInfoManager applicationInfoManager) {
+        this.applicationInfoManager = applicationInfoManager;
+    }
+
+    @EventListener
+    public void onGrpcStarted(GrpcServerStartedEvent event) {
+        String port = String.valueOf(event.getPort());
+        applicationInfoManager.registerAppMetadata(
+                Map.of(
+                        "gRPC_port", port,
+                        "grpcPort", port));
+    }
+}
