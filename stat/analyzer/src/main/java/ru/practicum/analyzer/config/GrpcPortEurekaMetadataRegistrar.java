88291df61baf0561cb82bd3aeededa313a93df7f
Oleg Digital <boss@agerasiadigital.ru>
2026-04-05 15:36:31 +0300

    jrpc fix

--- stat/analyzer/src/main/java/ru/practicum/analyzer/config/GrpcPortEurekaMetadataRegistrar.java
+++ stat/analyzer/src/main/java/ru/practicum/analyzer/config/GrpcPortEurekaMetadataRegistrar.java
@@ -2,6 +2,9 @@ package ru.practicum.analyzer.config;
 
 import com.netflix.appinfo.ApplicationInfoManager;
 import net.devh.boot.grpc.server.event.GrpcServerStartedEvent;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +14,16 @@ import java.util.Map;
 public class GrpcPortEurekaMetadataRegistrar {
 
     private final ApplicationInfoManager applicationInfoManager;
+    private final ObjectProvider<EurekaServiceRegistry> eurekaServiceRegistry;
+    private final ObjectProvider<EurekaRegistration> eurekaRegistration;
 
-    public GrpcPortEurekaMetadataRegistrar(ApplicationInfoManager applicationInfoManager) {
+    public GrpcPortEurekaMetadataRegistrar(
+            ApplicationInfoManager applicationInfoManager,
+            ObjectProvider<EurekaServiceRegistry> eurekaServiceRegistry,
+            ObjectProvider<EurekaRegistration> eurekaRegistration) {
         this.applicationInfoManager = applicationInfoManager;
+        this.eurekaServiceRegistry = eurekaServiceRegistry;
+        this.eurekaRegistration = eurekaRegistration;
     }
 
     @EventListener
@@ -23,5 +33,10 @@ public class GrpcPortEurekaMetadataRegistrar {
                 Map.of(
                         "gRPC_port", port,
                         "grpcPort", port));
+        EurekaServiceRegistry registry = eurekaServiceRegistry.getIfAvailable();
+        EurekaRegistration registration = eurekaRegistration.getIfAvailable();
+        if (registry != null && registration != null) {
+            registry.register(registration);
+        }
     }
 }
