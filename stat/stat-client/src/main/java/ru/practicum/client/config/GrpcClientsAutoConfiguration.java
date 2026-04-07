6192cb30172466666114e93af846652ddac28561
Oleg Digital <boss@agerasiadigital.ru>
2026-04-07 20:19:43 +0300

    Промежуточный fix

---
+++ stat/stat-client/src/main/java/ru/practicum/client/config/GrpcClientsAutoConfiguration.java
@@ -0,0 +1,9 @@
+package ru.practicum.client.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("ru.practicum.client")
+public class GrpcClientsAutoConfiguration {
+}
